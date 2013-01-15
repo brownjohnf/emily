72db689d43722ebc5c67da2db2bccc2bbe3c5057
John (Jack) Brown <jack@brownjohnf.com>
2013-01-15 11:59:03 -0800

    ready to test online

--- lib/emily.rb
+++ lib/emily.rb
@@ -6,14 +6,13 @@ module Emily
   class Application
   	def call(env)
   	  if env['PATH_INFO'] == '/favicon.ico'
-  	  	return [404, {'Content-Type' => 'text/html'}, []]
+  	  	return [404, { 'Content-Type' => 'text/html' }, []]
   	  end
 
   	  klass, act = get_controller_and_action(env)
   	  controller = klass.new(env)
   	  text = controller.send(act)
-  	  `echo debug > debug.txt`;
-        [200, { 'Content-Type' => 'text/html' }, [text]]
+  	  [200, { 'Content-Type' => 'text/html' }, [text]]
     end
   end
 
--- lib/emily/routing.rb
+++ lib/emily/routing.rb
@@ -1,12 +1,13 @@
 module Emily
   class Application
   	def get_controller_and_action(env)
-  	  _, cont, action, after = env["PATH_INFO"].split('/', 4)
-  	  cont = cont.downcase # "people"
-  	  cont[0] = cont[0].upcase # People
-  	  cont += "Controller" # PeopleController
+  	  _, controller, action, after = env["PATH_INFO"].split('/', 4)
+  	  controller = controller.downcase # "people"
+  	  controller[0] = controller[0].upcase # People
+  	  controller += "Controller" # PeopleController
+      STDERR.puts controller
 
-  	  [Object.const_get(cont), action]
+  	  [Object.const_get(controller), action]
   	end
   end
 end
